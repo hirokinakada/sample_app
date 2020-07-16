a7aef8d93b33ea60e03cff013a082eceb60acd8e
hirokinakada <dudu.hiccc@gmail.com>
2020-07-16 09:48:49 +0000

    first...

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'b368d50c3207af4d193436801a6d66f742577a6fd10bf7d3532f3b93c57d7b4ca41b5a496669e7eae8d0779b5baf05c7557e6165fef866644dff465f80747f18'
+
